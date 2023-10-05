1994f1398da880877c1b60ea5069b32716aa037c
William East <wjbetech@gmail.com>
2023-10-05 15:32:16 +0900

    Added q-params for dynamic naming with components

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 
 const Contact = () => {
+
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get("name");
+  
   return (
     <div>
-        <h2>Contact Page</h2>
+        <h2>Hey, {name}! Contact us here:</h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos illum est quis, consequatur porro minus impedit aut animi, qui sapiente a molestias eius illo? Deserunt pariatur officiis veniam et fuga?</p>
     </div>
   )
